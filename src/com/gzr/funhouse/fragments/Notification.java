ba5ccfe7c8fbd0f4f8377b4a04b5a14fcbc2544f
PMS22 <prathams99@rediff.com>
2018-09-07 13:43:04 +0530

    FWD_PORT: Add Flashlight blink on incoming calls [2/3]
    - Thanks to @shripal17 for suggesting list preference
      instead of a toggle
    - Add a device specific flashlight check

---
+++ src/com/gzr/funhouse/fragments/Notification.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2019 Validus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gzr.funhouse.fragments;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.os.Vibrator;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.util.gzosp.DeviceUtils;
+
+public class Notification extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
+
+    private ListPreference mFlashlightOnCall;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.notification);
+        ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mFlashlightOnCall = (ListPreference) findPreference(FLASHLIGHT_ON_CALL);
+        Preference FlashOnCall = findPreference("flashlight_on_call");
+        int flashlightValue = Settings.System.getInt(getContentResolver(),
+                Settings.System.FLASHLIGHT_ON_CALL, 0);
+        mFlashlightOnCall.setValue(String.valueOf(flashlightValue));
+        mFlashlightOnCall.setOnPreferenceChangeListener(this);
+
+        if (!DeviceUtils.deviceSupportsFlashLight(getActivity())) {
+            prefScreen.removePreference(FlashOnCall);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.VALIDUS;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mFlashlightOnCall) {
+               int flashlightValue = Integer.parseInt(((String) newValue).toString());
+               Settings.System.putInt(resolver,
+                     Settings.System.FLASHLIGHT_ON_CALL, flashlightValue);
+               mFlashlightOnCall.setValue(String.valueOf(flashlightValue));
+               return true;
+        }
+        return false;
+    }
+
+}
