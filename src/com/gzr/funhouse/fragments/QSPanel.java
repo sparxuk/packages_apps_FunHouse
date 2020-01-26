deec14e12e20d2183badca1c07ab4f14ce938796
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-26 17:43:23 +0900

    FunHouse: Introduce Adjustable Blur Intensity [2/2]
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- src/com/gzr/funhouse/fragments/QSPanel.java
+++ src/com/gzr/funhouse/fragments/QSPanel.java
@@ -44,11 +44,13 @@ public class QSPanel extends SettingsPreferenceFragment implements
     private static final String KEY_QS_PANEL_ALPHA = "qs_panel_alpha";
     private static final String QS_PANEL_COLOR = "qs_panel_color";
     private static final String QS_BLUR_ALPHA = "qs_blur_alpha";
+    private static final String QS_BLUR_INTENSITY = "qs_blur_intensity";
     static final int DEFAULT_QS_PANEL_COLOR = 0xffffffff;
 
     private ColorPickerPreference mQsPanelColor;
     private SystemSettingSeekBarPreference mQsPanelAlpha;
     private CustomSeekBarPreference mQSBlurAlpha;
+    private CustomSeekBarPreference mQSBlurIntensity;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -74,6 +76,12 @@ public class QSPanel extends SettingsPreferenceFragment implements
                 Settings.System.QS_BLUR_ALPHA, 100);
         mQSBlurAlpha.setValue(qsBlurAlpha);
         mQSBlurAlpha.setOnPreferenceChangeListener(this);
+
+        mQSBlurIntensity = (CustomSeekBarPreference) findPreference(QS_BLUR_INTENSITY);
+        int qsBlurIntensity = Settings.System.getInt(getContentResolver(),
+                Settings.System.QS_BLUR_INTENSITY, 30);
+        mQSBlurIntensity.setValue(qsBlurIntensity);
+        mQSBlurIntensity.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -101,6 +109,11 @@ public class QSPanel extends SettingsPreferenceFragment implements
             int value = (Integer) newValue;
             Settings.System.putInt(getContentResolver(),
                     Settings.System.QS_BLUR_ALPHA, value);
+            return true;
+        } else if (preference == mQSBlurIntensity) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.QS_BLUR_INTENSITY, value);
             return true;
         }
         return true;
